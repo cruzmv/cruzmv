362cf158587c1d2064a490173a50c198848d3e3b
mcruz <mauricio.cruz@altran.com>
2021-10-31 00:24:12 +0100

    first commit

---
+++ cruzmv/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cruzmv project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cruzmv.settings')
+
+application = get_wsgi_application()
