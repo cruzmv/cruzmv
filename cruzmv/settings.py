050a7f5bebea8ff45713146f2fcd49d28f910286
mcruz <mauricio.cruz@altran.com>
2021-10-31 15:19:00 +0000

    setting postgres

--- cruzmv/settings.py
+++ cruzmv/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'cruzmv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cruzmv',
+        'USER': 'postgres',
+        'PASSWORD': 'cruzmv142536',
+        'HOST': 'cruzmv.cthmamc4zrni.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
